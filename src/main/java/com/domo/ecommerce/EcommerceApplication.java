b76f710c42c688146250140e52212aaf67e35208
kdomo <dongho5088@naver.com>
2022-11-28 14:17:22 +0900

    feat : Redis를 session storage로 사용하기 위한 설정

--- src/main/java/com/domo/ecommerce/EcommerceApplication.java
+++ src/main/java/com/domo/ecommerce/EcommerceApplication.java
@@ -2,8 +2,10 @@ package com.domo.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
+@EnableRedisHttpSession
 public class EcommerceApplication {
 
     public static void main(String[] args) {
