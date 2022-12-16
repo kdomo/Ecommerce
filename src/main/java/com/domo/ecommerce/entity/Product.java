fd97f82a169f3c8f0c050114b24d3a45ee31ba2f
kdomo <dongho5088@naver.com>
2022-12-16 14:50:54 +0900

    [#13] feat : Product Entity 생성

---
+++ src/main/java/com/domo/ecommerce/entity/Product.java
@@ -0,0 +1,37 @@
+package com.domo.ecommerce.entity;
+
+import com.domo.ecommerce.common.entity.BaseEntity;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Where(clause = "deleted_at is null")
+@SQLDelete(sql = "UPDATE member SET deleted_at = CURRENT_TIMESTAMP WHERE id = ?")
+public class Product extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String simpleContent;
+
+    private String mainContent;
+
+    private int stock;
+
+    private int price;
+}
